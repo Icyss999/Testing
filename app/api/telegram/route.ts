1474fbe2bc00c7e51c0d1569693c87985ac495a2
Icyss999 <chungtaktest@gmail.com>
2026-06-29 23:07:54 +0700

    testing

--- app/api/telegram/route.ts
+++ app/api/telegram/route.ts
@@ -6,20 +6,21 @@ export const POST = async (req:Request)=>{
     const text = await req.json()
     const id = text.message.chat.id
     const user_text = text.message.text
+    console.log(id)
 
     let reply = ""
     switch (user_text){
         case  "/start" 
-        : reply = "Let's Begin!"
+        : reply = "What's up im from the api end point lolol"
         break
         case  "/order" 
-        : reply = "What is your order"
+        : reply = "What is your order, sir?"
         break
         default :
         reply = "Unknown Command!"
     }
 
-    await fetch (`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,{
+    fetch (`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,{
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
